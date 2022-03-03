972174f3ee0fd0b86d3b37c4a6169db6123fb8e6
Avalorian <avalorian2211@gmail.com>
2022-03-03 14:36:48 +0100

    Added New Webpages

--- main.js
+++ main.js
@@ -12,3 +12,11 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
